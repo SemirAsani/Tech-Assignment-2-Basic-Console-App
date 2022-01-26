9c6c214de8014d0564dfd9f0bffe9cf5ebff37b7
SemirAsani <semirasani@usf.edu>
2022-01-26 11:14:34 -0500

    quick fix

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Tech_Assignment_2_Basic_Console_App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I'm Semir Asani and this is my first Visual Studio assignment with a linked GitHub account!");
+            Console.WriteLine("I'm Semir Asani and this is my first Visual Studio assignment with a connected GitHub account.");
         }
     }
 }
