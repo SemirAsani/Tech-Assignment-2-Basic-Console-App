cc4d8ce3c8ccacaf659fe056e01a6158c74a91e9
SemirAsani <semirasani@usf.edu>
2022-01-26 10:58:52 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_Assignment_2_Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
